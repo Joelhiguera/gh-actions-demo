9a51f4eb056dbb62f28d33d9e98dd2082a9b9cfa
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-08 13:26:04 -0600

    create Footer compnent

--- Desktop/repos/week20-React-Portfolio/react-portfolio/src/App.js
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Aboutme from './components/Aboutme';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='header'>
       <Header />
       <Aboutme />
+      <Footer />
     </div>
   );
 }
--- Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Aboutme.js
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Aboutme.js
@@ -1,9 +1,11 @@
 
 const Aboutme = () => {
   return (
-    <div>
+    <div className="aboutMe">
       <h2>About Me:</h2>
-      <p>I am learning to become a full stack web developer. Please Hire me</p>
+      <p>I am learning to become a full stack web developer. Please Hire me. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis turpis nisi, eget efficitur mi tincidunt vel. Fusce aliquam ipsum id risus sodales, a ultricies turpis ullamcorper. Vivamus quis justo at metus vehicula auctor bibendum ut eros. Proin egestas blandit arcu sit amet fermentum. Nunc cursus cursus vulputate. Nam quis ipsum a neque vestibulum feugiat porttitor ut velit. Quisque nec congue nunc. Ut quis faucibus nisl, in fermentum leo. Donec suscipit enim at dictum volutpat.</p><br></br>
+
+      <p>Mauris erat tortor, vestibulum a felis eu, mattis commodo sem. Suspendisse suscipit leo feugiat ante blandit pellentesque. Sed aliquet diam a gravida fermentum. Morbi sed suscipit diam. Nullam magna nunc, molestie nec iaculis id, ultrices id dolor. Nam hendrerit finibus nisi in faucibus. Suspendisse fringilla molestie lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
     </div>
   )
 }
