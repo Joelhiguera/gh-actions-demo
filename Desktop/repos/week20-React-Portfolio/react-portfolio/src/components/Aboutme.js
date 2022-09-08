0cc0a0c4ce0383240b22eab8e363313283d14501
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-08 13:13:48 -0600

    create About me component

---
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Aboutme.js
@@ -0,0 +1,11 @@
+
+const Aboutme = () => {
+  return (
+    <div>
+      <h2>About Me:</h2>
+      <p>I am learning to become a full stack web developer. Please Hire me</p>
+    </div>
+  )
+}
+
+export default Aboutme
---
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Header.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+
+      <ul>
+        <li>Portfolio</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+
+      
+    </header>
+  )
+}
+
+export default Header
