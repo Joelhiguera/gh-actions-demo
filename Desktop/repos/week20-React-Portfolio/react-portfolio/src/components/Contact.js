a57c4cb2eee6b54358ae3f83e8ba312badb3c0f2
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-09 16:19:25 -0600

    creat const Contact in contact.js, import contact.js into app.js

---
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = () => {
+  return(
+    <>
+     <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+          <input type="submit" value="Submit" />
+        </form>
+    </>
+  )
+
+ 
+
+}
+
+export default Contact;
