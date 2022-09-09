1f6acf46fc1ed15166802754305c2fde1f507067
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-09 17:22:22 -0600

    delete email icon and add facebook icon in footer.js

--- Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Footer.js
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Footer.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import GithubIcon from '@material-ui/icons/GitHub'
-import EmailIcon from '@material-ui/icons/Email'
+import FacebookIcon from '@material-ui/icons/Facebook'
 const Footer = () => {
   return (
     <div className="footer">
       <div className='socialMedia'>
-      <LinkedInIcon />
-      <GithubIcon />
-      <EmailIcon />
+
+        <a href="https://www.linkedin.com/in/joel-higuera-877b74236/">
+          <LinkedInIcon />
+        </a>
+
+        <a href='https://github.com/joelhiguera'>
+          <GithubIcon />
+        </a>
+
+        <a href='https://facebook.com'>
+          <FacebookIcon />
+        </a>
+      
       </div>
      
     </div>
