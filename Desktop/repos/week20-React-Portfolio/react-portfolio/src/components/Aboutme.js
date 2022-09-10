cce5d756185e25fd6e7e644be17a09fa0b6173b2
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-10 09:21:15 -0600

    add hover css to footer icons

--- Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Aboutme.js
+++ Desktop/repos/week20-React-Portfolio/react-portfolio/src/components/Aboutme.js
@@ -1,7 +1,10 @@
-
+import React from 'react'
+import avatar from '../assets/projects/avatar.jpg'
 const Aboutme = () => {
   return (
     <div className="aboutMe">
+
+      <img id='avatar' src = {avatar} alt='avatar'></img>
 
       <p>I am learning to become a full stack web developer. Please Hire me. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis turpis nisi, eget efficitur mi tincidunt vel. Fusce aliquam ipsum id risus sodales, a ultricies turpis ullamcorper. Vivamus quis justo at metus vehicula auctor bibendum ut eros. Proin egestas blandit arcu sit amet fermentum. Nunc cursus cursus vulputate. Nam quis ipsum a neque vestibulum feugiat porttitor ut velit. Quisque nec congue nunc. Ut quis faucibus nisl, in fermentum leo. Donec suscipit enim at dictum volutpat.</p><br></br>
 
